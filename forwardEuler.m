a42faeed796a848bfacfd9cd950bf87c896c57dd
ldietsch <37944383+ldietsch@users.noreply.github.com>
2019-04-07 14:47:54 -0400

    Update to getStates to getStateDerivs

--- forwardEuler.m
+++ forwardEuler.m
@@ -3,7 +3,7 @@ function x_k1 = forwardEuler(x,u,Ts,info)
 %computes the approximate discretization of states for use in an NLMPC 
 %formulation.
 
-f_xk = getStates(x,u,info);
+f_xk = getStateDerivs(x,u,info);
 x_k1 = x + f_xk*Ts;
 
 
